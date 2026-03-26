260102cffcfe502e67aa9adb7b870b677c392cc5
g74c3 <graceharrisontate@aol.com>
2026-03-26 10:28:41 +0000

    bugs fixed, resolves #1

--- functions.py
+++ functions.py
@@ -2,10 +2,9 @@ def add(a, b):
     return a + b
 
 def subtract(a, b):
-    return a + b  
+    return a - b  
 
 def multiply(a, b):
     return a * b
 
-def convert_fahrenheit_to_celsius(fahrenheit):
-    return multiply(subtract(fahrenheit, 32), 9 / 5) # <-- Fix this in step 7
+
